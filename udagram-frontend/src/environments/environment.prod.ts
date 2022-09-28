656d0c05405513b1180a3dc1c7e2e32b1570a3c1
LONFO FOFE BLondeau <b.fofe@yahoo.com>
2022-09-28 07:04:12 +0100

    update fronten with reverceproxy

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae8c7edaf4e2a42f59b29333df88453a-590189512.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
